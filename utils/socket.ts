d821c4d0e720ec85d9750bfb78a084cd21cb7ac2
mferozmirza2005 <m.ferozmirza2005@gmail.com>
2024-08-23 21:16:59 +0500

    Fix cors issues.

--- utils/socket.ts
+++ utils/socket.ts
@@ -3,6 +3,8 @@ import { io, Socket } from "socket.io-client";
 const socket: Socket = io(process.env.NEXT_PUBLIC_SERVER_PATH as string, {
     autoConnect: false,
     reconnection: true,
+    withCredentials: true,
+    transports: ["websocket"],
 });
 
 export default socket;
